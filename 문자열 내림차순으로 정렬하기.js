4103d35f460f0ef6cb9ee10a8a15078094032b71
ljsoo123 <ljsoo123456@naver.com>
2020-12-18 19:06:52 +0900

    [CREATE] 문자열 내림차순으로 정렬하기(programmers)

---
+++ 문자열 내림차순으로 정렬하기.js
@@ -0,0 +1,8 @@
+function solution(s) {
+  return s.split("").sort().reverse().join("");
+  //split으로 문자열을 각각 배열의 인덱스로 쪼갠다.
+  //sort를 사용하여 오름차순으로 정렬한다.
+  //reverse를 사용하여 오름차순으로 정렬한 배열을 뒤집는다.
+  //join으로 쪼갠 배열을 다시 문자열로 합친다.
+}
+//링크 : https://programmers.co.kr/learn/courses/30/lessons/12917
